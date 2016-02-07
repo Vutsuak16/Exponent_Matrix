b56f08dc0bc9bdfb2951a90bf5aa8e31d6a4a3af
kaustuv deolal <vutsuak96@gmail.com>
2016-02-07 21:55:55 +0530

    another tree taken

---
+++ tree_adjacency_2.py
@@ -0,0 +1,163 @@
+import numpy as np
+
+n = 10
+
+a = [0 for i in range(n ** 2)]
+
+a = np.array(a, int)
+a = a.reshape((n, n))
+
+a[0][1] = 1
+a[1][0] = 1
+a[1][2] = 1
+a[1][3] = 1
+a[2][1] = 1
+a[3][1] = 1
+a[3][4] = 1
+a[3][5] = 1
+a[3][6] = 1
+a[4][3] = 1
+a[5][3] = 1
+a[6][3] = 1
+a[6][7] = 1
+a[6][8] = 1
+a[7][6] = 1
+a[8][6] = 1
+a[8][9] = 1
+a[9][8] = 1
+y = a
+ct = 100  # even on 1000 interations we dont get a positive matrix therefore we need to put self loops
+while ct > 0:
+    x = a
+    y = np.dot(x, y)
+    if 0 not in y:
+        print "exponent reached self loop not required"
+        break
+    ct -= 1
+
+'''a[0][0] = 1  # self loop put at vertex 1 , exponent is 8
+y = a
+ct = 0
+while ct != 1000:
+    x = a
+    y = np.dot(x, y)
+    if 0 not in y:
+        print "exponent is " + str(ct)
+        print y
+        break
+    ct += 1
+'''
+'''a[1][1] = 1  # self loop put at vertex 2 exponent is 6
+y = a
+ct = 0
+while ct != 1000:
+    x = a
+    y = np.dot(x, y)
+    if 0 not in y:
+        print "exponent is " + str(ct)
+        print y
+        break
+    ct += 1
+'''
+'''
+a[2][2] = 1  # self loop put at vertex 3 exponent is 8
+y = a
+ct = 0
+while ct != 1000:
+    x = a
+    y = np.dot(x, y)
+    if 0 not in y:
+        print "exponent is " + str(ct)
+        print y
+        break
+    ct += 1
+'''
+'''
+a[3][3] = 1  # self loop put at vertex 4 exponent is 4
+y = a
+ct = 0
+while ct != 1000:
+    x = a
+    y = np.dot(x, y)
+    if 0 not in y:
+        print "exponent is " + str(ct)
+        print y
+        break
+    ct += 1
+'''
+'''
+a[4][4] = 1  # self loop put at vertex 5 exponent is 6
+y = a
+ct = 0
+while ct != 1000:
+    x = a
+    y = np.dot(x, y)
+
+    if 0 not in y:
+        print "exponent is " + str(ct)
+        print y
+        break
+    ct += 1
+'''
+'''a[5][5] = 1  # self loop put at vertex 6 exponent is 6
+y = a
+ct = 0
+while ct != 1000:
+    x = a
+    y = np.dot(x, y)
+    if 0 not in y:
+        print "exponent is " + str(ct)
+        print y
+        break
+    ct += 1
+'''
+'''a[6][6] = 1  # self loop put at vertex 7 exponent is 4
+y = a
+ct = 0
+while ct != 100:
+    x = a
+    y = np.dot(x, y)
+    if 0 not in y:
+        print "exponent is " + str(ct)
+        print y
+        break
+    ct += 1'''
+
+'''a[7][7] = 1  # self loop put at vertex 8 exponent is 6
+y = a
+ct = 0
+while ct != 100:
+    x = a
+    y = np.dot(x, y)
+    if 0 not in y:
+        print "exponent is " + str(ct)
+        print y
+        break
+    ct += 1
+'''
+'''
+a[8][8] = 1  # self loop put at vertex 9 exponent is 6
+y = a
+ct = 0
+while ct != 100:
+    x = a
+    y = np.dot(x, y)
+    if 0 not in y:
+        print "exponent is " + str(ct)
+        print y
+        break
+    ct += 1
+'''
+'''
+a[9][9] = 1  # self loop put at vertex 10 exponent 8
+y = a
+ct = 0
+while ct != 100:
+    x = a
+    y = np.dot(x, y)
+    if 0 not in y:
+        print "exponent is " + str(ct)
+        print y
+        break
+    ct += 1
+'''
